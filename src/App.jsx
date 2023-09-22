f135b6e5bbc6830f96a151be061824841580b777
hoshikwyw <khaingwutyiwin1712@gmail.com>
2023-09-22 15:47:10 +0630

    home frame set

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import Path from './redux/Path'
+import React from "react";
+import Path from "./redux/Path";
+import Navbar from "./components/Navbar";
+import SideBar from "./components/SideBar";
+import Footer from "./components/Footer";
+import "./components/style.css"
 
 const App = () => {
   return (
-    <div>
-      <Path />
+    <div className=" relative flex">
+      <div className=" bg-amber-200 w-full md:w-[200px] lg:w-[300px] sidebar">
+        <SideBar />
+      </div>
+      <div className=" bg-slate-50">
+        <div className=" bg-slate-400">
+          <Navbar />
+        </div>
+        <div className=" bg-red-300">
+          <Path />
+        </div>
+        <div className=" bg-green-200">
+          <Footer />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,23 @@
 import React from "react";
-import Navbar from "../components/Navbar";
 import HomeCard from "../components/HomeCard";
 import { Link } from "react-router-dom";
 import { links } from "../assets/PagesLinks";
 
 const Home = () => {
   return (
-    <div className=" flex justify-center items-center flex-col w-full">
-      <Navbar />
-      <div className=" flex flex-wrap gap-5 mt-5 mx-5 justify-center">
-        {links?.map((page) => (
-          <div className="">
-            <Link
-              to={page?.to}
-              className=" flex flex-col w-full justify-center items-center">
-              <HomeCard />
-              <button className=" bg-slate-500 px-3 py-1 rounded-md">
-                {page?.linkName}
-              </button>
-            </Link>
-          </div>
-        ))}
-      </div>
+    <div className=" flex flex-wrap gap-5 py-5 items-center justify-center">
+      {links?.map((page) => (
+        <div className="">
+          <Link
+            to={page?.to}
+            className=" flex flex-col w-full justify-center items-center">
+            <HomeCard />
+            <button className=" bg-slate-500 px-3 py-1 rounded-md">
+              {page?.linkName}
+            </button>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
