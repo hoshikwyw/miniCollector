0100cd76f49eefd798574c3a82f8f313100c72dd
hoshikwyw <khaingwutyiwin1712@gmail.com>
2023-10-19 22:03:07 +0630

    weather layout setup

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import {FcSearch} from "react-icons/fc"
 
 const Weather = () => {
   return (
-    <div className=' bg-slate-400 w-full md:w-[50%]'>
-      Weather
+    <div className=' bgShadow w-full md:w-[50%] flex flex-col justify-center items-center p-3'>
+      <div className=' flex justify-center items-center gap-3'>
+        <input type="text" name="" id="" className=" bg-[#ffffff] rounded-xl outline-none px-3 py-1" placeholder='Search City . . .' />
+        <h2 className=' text-xl'><FcSearch /></h2>
+      </div>
+      <div className=' flex w-full justify-between px-5 py-3 items-center text-white'>
+      <div>
+        <h2>City name</h2>
+        <h2>temperature</h2>
+      </div>
+      <div className="">
+        <h2>weather icon</h2>
+        <h2>weather condition</h2>
+      </div>
+      </div>
     </div>
   )
 }
