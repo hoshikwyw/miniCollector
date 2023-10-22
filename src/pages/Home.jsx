60cdb0f13ce133522c93209014a017bb54624cd5
hoshikwyw <khaingwutyiwin1712@gmail.com>
2023-10-22 18:34:58 +0630

    btn color change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import HomeCard from "../components/HomeCard";
 import { Link } from "react-router-dom";
 import { links } from "../assets/PagesLinks";
-import "../App.css"
+import "../App.css";
 import Clock from "../components/Clock";
 import Weather from "../components/Weather";
 
@@ -13,20 +13,20 @@ const Home = () => {
         <Clock />
         <Weather />
       </div>
-    <div className=" flex flex-wrap gap-5 py-5 items-center justify-center">
-      {links?.map((page) => (
-        <div className="">
-          <Link
-            to={page?.to}
-            className=" flex flex-col w-full justify-center items-center">
-            <HomeCard image={page?.image} />
-            <button className=" px-3 py-1 text-white font-semibold tracking-widest bgShadow mt-3">
-              {page?.linkName}
-            </button>
-          </Link>
-        </div>
-      ))}
-    </div>
+      <div className=" flex flex-wrap gap-5 py-5 items-center justify-center">
+        {links?.map((page) => (
+          <div className="">
+            <Link
+              to={page?.to}
+              className=" flex flex-col w-full justify-center items-center">
+              <HomeCard image={page?.image} />
+              <button className=" px-3 py-1 text-white font-semibold tracking-widest bg-blue-400 rounded-xl mt-3">
+                {page?.linkName}
+              </button>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
