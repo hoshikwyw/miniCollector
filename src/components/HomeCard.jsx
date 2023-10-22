c2a77ca22bdfc1584657e9092c36dc0ba14f4926
hoshikwyw <khaingwutyiwin1712@gmail.com>
2023-10-22 18:16:53 +0630

    home hover add

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { links } from "../assets/PagesLinks";
 
-const HomeCard = ({image}) => {
+const HomeCard = ({ image }) => {
   // console.log(links);
   return (
-    <div className=" w-[200px] h-[200px] bgShadow">
-      <img src={image} alt="" className=" w-[180px] h-[180px] rounded-2xl mx-auto mt-[10px] brightness-50" />
+    <div className=" w-[200px] h-[200px] bgShadow group">
+      <div className=" w-[180px] h-[180px] overflow-hidden mx-auto mt-[10px] rounded-2xl">
+        <img
+          src={image}
+          alt=""
+          className=" w-[180px] h-[180px] rounded-2xl brightness-50 group-hover:brightness-90 group-hover:scale-105 "
+        />
+      </div>
     </div>
   );
 };
