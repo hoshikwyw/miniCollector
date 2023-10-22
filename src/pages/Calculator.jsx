77325b7ce74edd3df0094294779ad6ff56a95818
hoshikwyw <khaingwutyiwin1712@gmail.com>
2023-10-22 22:39:47 +0630

    calculator ui raw add

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Calculator = () => {
-
   const [state, setState] = useState({
-    currentValue: '0',
+    currentValue: "0",
     operator: null,
     previousValue: null,
   });
@@ -11,7 +10,8 @@ const Calculator = () => {
   const handleNumber = (value) => {
     setState((prevState) => ({
       ...prevState,
-      currentValue: prevState.currentValue === '0' ? value : prevState.currentValue + value,
+      currentValue:
+        prevState.currentValue === "0" ? value : prevState.currentValue + value,
     }));
   };
 
@@ -27,28 +27,32 @@ const Calculator = () => {
     const { currentValue, operator, previousValue } = state;
 
     switch (operator) {
-      case '+':
+      case "+":
         setState((prevState) => ({
           ...prevState,
-          currentValue: Number(prevState.currentValue) + Number(prevState.previousValue),
+          currentValue:
+            Number(prevState.currentValue) + Number(prevState.previousValue),
         }));
         break;
-      case '-':
+      case "-":
         setState((prevState) => ({
           ...prevState,
-          currentValue: Number(prevState.previousValue) - Number(prevState.currentValue),
+          currentValue:
+            Number(prevState.previousValue) - Number(prevState.currentValue),
         }));
         break;
-      case '*':
+      case "*":
         setState((prevState) => ({
           ...prevState,
-          currentValue: Number(prevState.previousValue) * Number(prevState.currentValue),
+          currentValue:
+            Number(prevState.previousValue) * Number(prevState.currentValue),
         }));
         break;
-      case '/':
+      case "/":
         setState((prevState) => ({
           ...prevState,
-          currentValue: Number(prevState.previousValue) / Number(prevState.currentValue),
+          currentValue:
+            Number(prevState.previousValue) / Number(prevState.currentValue),
         }));
         break;
       default:
@@ -58,33 +62,48 @@ const Calculator = () => {
 
   const handleClear = () => {
     setState({
-      currentValue: '0',
+      currentValue: "0",
       operator: null,
       previousValue: null,
     });
   };
 
   return (
-    <div>
-      <input type="text" value={state.currentValue} readOnly />
-      <button onClick={() => handleNumber('1')}>1</button>
-      <button onClick={() => handleNumber('2')}>2</button>
-      <button onClick={() => handleNumber('3')}>3</button>
-      <button onClick={() => handleOperator('+')}>+</button>
-      <button onClick={() => handleNumber('4')}>4</button>
-      <button onClick={() => handleNumber('5')}>5</button>
-      <button onClick={() => handleNumber('6')}>6</button>
-      <button onClick={() => handleOperator('-')}>-</button>
-      <button onClick={() => handleNumber('7')}>7</button>
-      <button onClick={() => handleNumber('8')}>8</button>
-      <button onClick={() => handleNumber('9')}>9</button>
-      <button onClick={() => handleOperator('*')}>*</button>
-      <button onClick={() => handleClear()}>C</button>
-      <button onClick={() => handleNumber('0')}>0</button>
-      <button onClick={() => handleEqual()}>=</button>
-      <button onClick={() => handleOperator('/')}>/</button>
+    <div className=" min-h-screen bgShadow w-[50%] mx-auto m-5 py-3">
+      <input
+        type="text"
+        value={state.currentValue}
+        readOnly
+        className=" w-[80%] mx-auto flex justify-center items-center px-3 py-1"
+      />
+      <div className=" flex flex-col text-xl gap-2 w-[90%] mx-auto">
+        <div className=" flex justify-between">
+          <button onClick={() => handleNumber("1")}>1</button>
+          <button onClick={() => handleNumber("2")}>2</button>
+          <button onClick={() => handleNumber("3")}>3</button>
+          <button onClick={() => handleOperator("+")}>+</button>
+        </div>
+        <div className=" flex justify-between">
+          <button onClick={() => handleNumber("4")}>4</button>
+          <button onClick={() => handleNumber("5")}>5</button>
+          <button onClick={() => handleNumber("6")}>6</button>
+          <button onClick={() => handleOperator("-")}>-</button>
+        </div>
+        <div className=" flex justify-between">
+          <button onClick={() => handleNumber("7")}>7</button>
+          <button onClick={() => handleNumber("8")}>8</button>
+          <button onClick={() => handleNumber("9")}>9</button>
+          <button onClick={() => handleOperator("*")}>*</button>
+        </div>
+        <div className=" flex justify-between">
+          <button onClick={() => handleClear()}>C</button>
+          <button onClick={() => handleNumber("0")}>0</button>
+          <button onClick={() => handleEqual()}>=</button>
+          <button onClick={() => handleOperator("/")}>/</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Calculator
+export default Calculator;
