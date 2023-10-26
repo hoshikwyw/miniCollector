f6d00007bff4195d47bde8c25e146e7fd56a29c5
KhaingWutYiWin <113814930+hoshikwyw@users.noreply.github.com>
2023-10-26 12:57:21 +0000

    profile text add

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div>
-      Profile
+    <div className=' min-h:screen flex justify-center items-center'>
+      <h2>Hi this is profile page</h2>
     </div>
   )
 }
