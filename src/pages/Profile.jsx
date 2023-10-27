55f3a47848d05ae34e01c92bf47da3b06d28c083
KhaingWutYiWin <113814930+hoshikwyw@users.noreply.github.com>
2023-10-27 15:29:21 +0000

    codespace error clear

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,48 +3,9 @@ import React from 'react'
 const Profile = () => {
   return (
     <div className=' min-h:screen flex justify-center items-center'>
-      <h2>Hi this is profile page</h2h2hh2hh2h2h2h2h2h2hh2h2hhh2h2h2h2h2h2h2hh2h2
-h2h2h2h2h2h2h2hh2h2hhh2h2h2h2h2h2h2hh2h2hhh2h
-2
-2
-h2hh2h2h2h2h2h2h2hh2h2hhh2hh2h2h2h2h2h2h2hh2h2hhh
-hh2hh2h2h2h2h2h2h2hh2h2hhh2hh2hh2h2h2h2h2h2h2hh2h2hhh2
-h2h
+      <h2>Hi this is profile page</h2>
+      <h2>Hi this is writing from codespace</h2>
 
-h2h2h2h2hh>
-h2h
-
- h2h2h2hh2h2h2h2h2hh2h2h2h2h
-
-h2h2h2h2hh>
-h2h2h22h2h2hh2h2h2h2hh>
-h2h
-
-2h2h2hh2h2h2h2hh>
-h2h
-
-h22hh22hh22hh22h
-2h
-h
-
-h2h2h2h2hh>
-h2h
-hhh2h2h2hh2hhhhhhhhhhhh2h2h2hh2hhhhhhhhh
- h2h2h2hh2hh2h2hh2hh>
-hhhhhhhhhhhhhh2h2hh2hhh2h2h2hh2hhhhhhhhhhhhhhhhhh2h2hh2hhh2h2h2hh2hhhh2h2hh2hhhhh2h2hh2hhh2h2h2hh2hhhh>
-2h2h2hh2hhh2h2h2hh2hhhh
-2h2h2hh2hhh2h2h2hh2hhhh
-h2h2h2hh2hhh2h2h2hh2hh
-hh2h2h2hh2hhh2h2h2hhhh2h2hhh2h2h2hh2hhh2h2h2hhhh2h2h2hh2hhh2h2h2hh222hh2hhh2h2h2hh22
-hhhhhhhhhhhhh2h2h2hh2hhhhhhhhhhhhhh2h2hhhhhhhhhhhhhh2h2h2hh2hh2h2h2hh2hhhhhhhhhh
-hhhhhhhhhh2h2h2hh2hhhhhhhhhhh2h2h2hh2h
-hhhhh2h2h2hh2hhh2h
-hhhhhh2h2h2hh2hhh
-hhhhhhh2h2h2hh2h
-hhhhhhhhhhhhhh2h2hh2hhh2h2h2hh2hhhhhhhhhh2h2h2hh2h
-hhhhhhhhh2h2h2
-hhhhhhhhhhhhhhhhhh
-hhhhhhhh
 
     </div>
   )
