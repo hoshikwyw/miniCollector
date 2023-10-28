6cf79c503fa8ce7818112c44948f58b61d96d164
KhaingWutYiWin <113814930+hoshikwyw@users.noreply.github.com>
2023-10-28 11:31:54 +0000

    day3 from codespace

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,6 +5,7 @@ const Profile = () => {
     <div className=' min-h:screen flex justify-center items-center'>
       <h2>Hi this is profile page</h2>
       <h2>Hi this is writing from codespace</h2>
+      <h2>Hi this is day3 coding from mobile codespace</h2>
 
 
     </div>
