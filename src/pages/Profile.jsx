711497ef8b69f27b847045427a19d031a6911519
KhaingWutYiWin <113814930+hoshikwyw@users.noreply.github.com>
2023-10-31 15:16:56 +0000

    day6 from mobile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -8,7 +8,7 @@ const Profile = () => {
       <h2>Hi this is day3 coding from mobile codespace</h2>
 <h1>Hi this is day 4 coding from mobile codespace</h1>
 <h2>hi this is day 5 coding from mobile codespace..</h2>
-
+<h1>hi this is day 6 from mobile codespace and today is the last day</h1>
     </div>
   )
 }
