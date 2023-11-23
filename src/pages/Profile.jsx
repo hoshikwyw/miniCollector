a4021eae9245dca3d5d9a596a6b91f2ff50bbc4b
hoshikwyw <khaingwutyiwin1712@gmail.com>
2023-11-23 22:09:51 +0630

    img add profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,12 +3,12 @@ import pfp from "../MemoryImages/card1.jpg"
 
 const Profile = () => {
   return (
-    <div className=' min-h:screen flex w-[95%] mx-auto mt-5'>
-      <div className=' bgShadow h-[400px] w-[50%]'>
-        Hello
-        
+    <div className=' min-h:screen flex w-[95%] gap-[20px] mx-auto mt-5'>
+      <div className=' bgShadow h-[400px] w-[30%] p-5'>
+        <img src={pfp} alt="" className=' w-20 h-20 rounded-full' />
+        <h2>Hello</h2>
       </div>
-      <div className=" bgShadow h-[400px] w-[50%]">
+      <div className=" bgShadow h-[400px] w-[80%]">
         
       </div>
     </div>
