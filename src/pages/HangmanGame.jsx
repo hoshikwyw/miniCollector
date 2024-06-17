69d8b73ddad93311e73270fb1d7a4a00a983452f
KhaingWutYiWin <113814930+hoshikwyw@users.noreply.github.com>
2024-06-17 08:15:27 +0630

    Hangman ui change

--- src/pages/HangmanGame.jsx
+++ src/pages/HangmanGame.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import SubNavBar from '../components/SubNavBar'
 
 const HangmanGame = () => {
-  return (
-    <div>
-      hangman game
-    </div>
-  )
+    return (
+        <>
+            <SubNavBar title="hangman game" />
+            <div className=' min-h-[calc(100vh-300px)] bgShadow w-[80%] mx-auto m-5 py-3 px-5'>
+                <div className=" w-full flex justify-center items-center bg-orange-300 h-[calc(100vh-213px)]">
+                    <div className=" modal w-[60%] bg-white/50 rounded-lg h-[50%] flex justify-center">
+                        <h1>Game Over!!</h1>
+                    </div>
+                </div>
+                <div className="imgSec">
+                    <img src="./" alt="" />
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default HangmanGame
